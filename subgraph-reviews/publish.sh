e3654ab370abd567b96b4aa7841f3f2273fd3ad7
John William Sheffield Jr <john@sheffinet.org>
2023-01-01 22:35:40 +0000

    Update publish.sh

--- subgraph-reviews/publish.sh
+++ subgraph-reviews/publish.sh
@@ -2,4 +2,4 @@ export $(xargs < ../router/.env) && \
   rover subgraph publish $APOLLO_GRAPH_REF \
   --name reviews \
   --schema ./reviews.graphql \
-  --routing-url https://johnsheffinet-studious-disco-7p6vqwpw6gcxg5w-4002.preview.app.github.dev/
+  --routing-url https://johnsheffinet-legendary-eureka-wgpr6xg496c9764-4002.preview.app.github.dev/
